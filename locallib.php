5fbbfaedbdb581d2e2a7c6e4225018362422faab
root <iseki@rsch.tuis.ac.jp>
2025-02-04 19:04:42 +0900

    print_error

--- locallib.php
+++ locallib.php
@@ -63,7 +63,7 @@ function  lticontainer_join_custom_params($custom_data)
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-function  ltictr_print_error($message, $mname=null, $url=null)
+function  ltictr_print_error($message, $mname='', $url='')
 {
     throw new \moodle_exception($message, $mname, $url);
     exit;
